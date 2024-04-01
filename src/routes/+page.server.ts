da230205550bed330be1d5e498da6e59455767bf
Ruán <ruan@murgatroyd.me>
2024-04-01 01:17:41 +0100

    New DB Schema, favicon, updating starboard layout

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -20,7 +20,6 @@ export const load = (async () => {
         parsedStars.push(stareeTemplate);
     }
 
-    console.log(parsedStars);
     return {
         starboard: parsedStars
     };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,12 @@
     export let data: PageData;
 </script>
 
+<svelte:head>
+    <title>
+        Starboard
+    </title>
+</svelte:head>
+
 <div class="header">
     <h1 class="title large">
         Ruán's Starboard
@@ -39,85 +45,102 @@
 </div>
 
 <div class="starlist">
-    {#each data.starboard as {name, stars}, i}
+    {#if data.starboard.length == 0}
         <div class="starbox">
-            <div class="positionbox">
-                <h1 class="title medium">
-                    {i + 1}
-                </h1>
-            </div>
             <div class="staree">
                 <h1 class="staree-name">
-                    {name} -
-                    {#if stars.total < 0}
-                        <span style="color: var(--red-500);">
-                            {stars.total}
-                        </span>
-                    {:else}
-                        <span>
-                            {stars.total}
-                        </span>
-                    {/if}
+                    No people on starboard...
                 </h1>
-
-                <div class="staree-stars">
-                    {#if stars.total >= 0}
-                        {#if stars.hundredStars >= 0}
-                            {#each Array(Number(stars.hundredStars)) as _, i}
-                                <TrophyFilled fill="var(--yellow-500)" size={32}/>
-                            {/each}
-                        {/if}
-                        {#if stars.fiftyStars >= 0}
-                            {#each Array(Number(stars.fiftyStars)) as _, i}
-                                <Trophy fill="var(--yellow-500)" size={32}/>
-                            {/each}
-                        {/if}
-                        {#if stars.tenStars >= 0}
-                            {#each Array(Number(stars.tenStars)) as _, i}
-                                <StarFilled fill="var(--yellow-500)" size={32}/>
-                            {/each}
-                        {/if}
-                        {#if stars.fiveStars >= 0}
-                            {#each Array(Number(stars.fiveStars)) as _, i}
-                                <StarHalf fill="var(--yellow-500)" size={32}/>
-                            {/each}
-                        {/if}
-                        {#if stars.stars >= 0}
-                            {#each Array(Number(stars.stars)) as _, i}
-                                <Star fill="var(--yellow-500)" size={32}/>
-                            {/each}
-                        {/if}
-                    {:else if stars.total < 0}
-                        {#if stars.hundredStars >= 0}
-                            {#each Array(Number(stars.hundredStars)) as _, i}
-                                <TrophyFilled fill="var(--red-500)" size={32}/>
-                            {/each}
-                        {/if}
-                        {#if stars.fiftyStars >= 0}
-                            {#each Array(Number(stars.fiftyStars)) as _, i}
-                                <Trophy fill="var(--red-500)" size={32}/>
-                            {/each}
-                        {/if}
-                        {#if stars.tenStars >= 0}
-                            {#each Array(Number(stars.tenStars)) as _, i}
-                                <StarFilled fill="var(--red-500)" size={32}/>
-                            {/each}
-                        {/if}
-                        {#if stars.fiveStars >= 0}
-                            {#each Array(Number(stars.fiveStars)) as _, i}
-                                <StarHalf fill="var(--red-500)" size={32}/>
-                            {/each}
-                        {/if}
-                        {#if stars.stars >= 0}
-                            {#each Array(Number(stars.stars)) as _, i}
-                                <Star fill="var(--red-500)" size={32}/>
-                            {/each}
-                        {/if}
-                    {:else}
-                        <p>Unknown</p>
-                    {/if}
-                </div>
             </div>
         </div>
-    {/each}
+    {:else}
+        {#each data.starboard as {name, stars}, i}
+            <div class="starbox">
+                <div class="positionbox">
+                    <h1 class="title medium">
+                        {i + 1}
+                    </h1>
+
+                </div>
+                <div class="staree">
+                    <h1 class="staree-name">
+                        {name}:
+                        {#if stars.total < 0}
+                            <span style="color: var(--red-500);">
+                                {stars.total}
+                            </span>
+                        {:else}
+                            <span>
+                                {stars.total}
+                            </span>
+                        {/if}
+
+                        {#if stars.total == 1 || stars.total == -1}
+                            star
+                        {:else}
+                            stars
+                        {/if}
+                    </h1>
+
+                    <div class="staree-stars">
+                        {#if stars.total >= 0}
+                            {#if stars.hundredStars >= 0}
+                                {#each Array(Number(stars.hundredStars)) as _, i}
+                                    <TrophyFilled fill="var(--yellow-500)" size={32}/>
+                                {/each}
+                            {/if}
+                            {#if stars.fiftyStars >= 0}
+                                {#each Array(Number(stars.fiftyStars)) as _, i}
+                                    <Trophy fill="var(--yellow-500)" size={32}/>
+                                {/each}
+                            {/if}
+                            {#if stars.tenStars >= 0}
+                                {#each Array(Number(stars.tenStars)) as _, i}
+                                    <StarFilled fill="var(--yellow-500)" size={32}/>
+                                {/each}
+                            {/if}
+                            {#if stars.fiveStars >= 0}
+                                {#each Array(Number(stars.fiveStars)) as _, i}
+                                    <StarHalf fill="var(--yellow-500)" size={32}/>
+                                {/each}
+                            {/if}
+                            {#if stars.stars >= 0}
+                                {#each Array(Number(stars.stars)) as _, i}
+                                    <Star fill="var(--yellow-500)" size={32}/>
+                                {/each}
+                            {/if}
+                        {:else if stars.total < 0}
+                            {#if stars.hundredStars >= 0}
+                                {#each Array(Number(stars.hundredStars)) as _, i}
+                                    <TrophyFilled fill="var(--red-500)" size={32}/>
+                                {/each}
+                            {/if}
+                            {#if stars.fiftyStars >= 0}
+                                {#each Array(Number(stars.fiftyStars)) as _, i}
+                                    <Trophy fill="var(--red-500)" size={32}/>
+                                {/each}
+                            {/if}
+                            {#if stars.tenStars >= 0}
+                                {#each Array(Number(stars.tenStars)) as _, i}
+                                    <StarFilled fill="var(--red-500)" size={32}/>
+                                {/each}
+                            {/if}
+                            {#if stars.fiveStars >= 0}
+                                {#each Array(Number(stars.fiveStars)) as _, i}
+                                    <StarHalf fill="var(--red-500)" size={32}/>
+                                {/each}
+                            {/if}
+                            {#if stars.stars >= 0}
+                                {#each Array(Number(stars.stars)) as _, i}
+                                    <Star fill="var(--red-500)" size={32}/>
+                                {/each}
+                            {/if}
+                        {:else}
+                            <p>Unknown</p>
+                        {/if}
+                    </div>
+                </div>
+            </div>
+        {/each}
+    {/if}
 </div>
