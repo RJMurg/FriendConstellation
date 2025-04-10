34a08cfe9ad42f3062b8d66799682291b06b6b78
Ruán <ruan@murgatroyd.me>
2025-04-10 14:11:45 +0100

    asdasdas

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 # Wait for the database to be ready
 echo "Waiting for DB to be ready..."
-until nc -z starboard-socs-db 5432; do
+until nc -z starboard-db 5432; do
   sleep 1
 done
 echo "DB is ready."
